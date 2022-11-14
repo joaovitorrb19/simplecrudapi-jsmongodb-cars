c7453c1d8a30995a3500a9bc1665d93352960869
Joao Vitor <joaovitor.rb19@gmail.com>
2022-11-14 07:57:22 -0300

    adicionado as pastas, instalado o module mongoose, criado o primeiro script para começar a configurar o servidor http

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log('ola')
+import express from 'express'
+import mongoose from 'mongoose'
+
+const server = express()
+const database = mongoose.connect('mongodb://joao:teste@localhost:27017/admin')
+
+server.listen(3000)
+
+
