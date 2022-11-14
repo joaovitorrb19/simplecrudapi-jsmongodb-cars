174a16ac80131eff520fe318960e5afe45054aca
Joao Vitor <joaovitor.rb19@gmail.com>
2022-11-14 11:44:30 -0300

    adicionado varias coisas

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import router from './router'
 
 const server = express()
 const database = mongoose.connect('mongodb://joao:teste@localhost:27017/admin')
+.then((data)=>{
+    console.log('Conectou com sucesso ao mongodb!')
+})
+.catch((err) => {
+    console.log('erro ao connectar com o banco:' , err.message)
+})
+
+
+server.use(router)
+server.use(express.json)
 
 server.listen(3000)
 
